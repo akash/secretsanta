eb131066d17e1c68f724f1543d28ab91bec81292
akash <akash@thoughtworks.com>
2013-06-11 17:55:45 +0300

    initialising project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Secretsanta::Application.config.secret_token = '33247e7657468b2996b996634ab11dc517cc599e6817ad6ce8f73004db2be22158d1300b4a122def575e92f84e5938651d26181ae55e275e78999e7d0afcd107'
