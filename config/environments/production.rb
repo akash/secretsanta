93b367e95d702bc8b8f0e6c2a96c441b25ad22bb
akash <akash@thoughtworks.com>
2013-06-25 15:39:41 +0300

    attempting to fix sendmailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ Secretsanta::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+	config.action_mailer.default_url_options = { :host => 'santas-lil-helper.herokuapp.com' }
 
   # Enable threaded mode
   # config.threadsafe!
