585129f63416f755f8ad13db93b2e349970ba661
akash <akash@thoughtworks.com>
2013-06-26 10:59:50 +0300

    ignoring ill formed email address when inviting

--- app/controllers/admin/invitations_controller.rb
+++ app/controllers/admin/invitations_controller.rb
@@ -1,7 +1,10 @@
 class Admin::InvitationsController < Admin::AdminController
 
+	EMAIL_REGEXP = /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i
+
   def create
-		invitees = params[:invitees]
+		invitees = params[:invitees].select {|invitee| EMAIL_REGEXP.match(invitee) }
+
 		party_id = params[:party_id]
 		invitees.each {|invitee|  UserMailer.invite(current_admin, invitee, new_user_party_registration_url(party_id)).deliver}
 
