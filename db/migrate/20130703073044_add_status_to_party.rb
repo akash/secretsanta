bdb24fd5b2a26e8f6f82f715bf966798b4c99157
akash <akash@thoughtworks.com>
2013-11-16 17:44:54 +0000

    fixing broken migration

--- db/migrate/20130703073044_add_status_to_party.rb
+++ db/migrate/20130703073044_add_status_to_party.rb
@@ -1,5 +1,5 @@
 class AddStatusToParty < ActiveRecord::Migration
   def change
-		add_column :parties, :status, :string, :default => Party::STATUS[:pre_launch]
+		add_column :parties, :status, :string, :default => Admin::STATUS[:pre_launch]
   end
 end
